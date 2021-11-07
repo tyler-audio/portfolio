7caf9c9a17426d09b91c2eb4c20c46e26a3065de
Tyler Jones <tylerj.audio94@gmail.com>
2021-11-07 03:53:03 -0800

    Basic project data added, missing url links

---
+++ components/lib/projectData.js
@@ -0,0 +1,23 @@
+export default {
+  fulcrum: {
+    title: 'Fulcrum',
+    tagline: 'An interactive musical web app designed to play various sampled sounds',
+    description: 'Fulcrum is a loop-based musical step sequencer inspired by existing software and hardware units. Users are able to choose from a selection of various samples and program patterns that will then trigger the selected sounds in time. Each sound has its own various channel controls that can be used to perform a rough mix on the music. Tonal samples open a interactive piano roll when selected, which allow users to play those samples as an instrument. This project was completed during my experience in Hack Reactor as a part of a two-day MVP sprint.',
+    github: 'https://github.com/tyler-audio/fulcrum',
+    url: '',
+  },
+  neighbr: {
+    title: 'Neighbr',
+    tagline: 'A social media platform connecting users through local communities',
+    description: 'Neighbr is a community driven forum enabling users to connect and interact with fellow community members; sharing ideas, planning events, exchanging items, requesting favors and more through the inclusion of private messaging and location sharing features.',
+    github: 'https://github.com/Cerulean-Sea/Neighbr',
+    url: '',
+  },
+  catwalk: {
+    title: 'Catwalk',
+    tagline: 'A single page web app displaying products for an online apparel store',
+    description: 'Project Catwalk was developed by a small team of engineers as an e-commerce platform, designed to allow users to browse and read details on various products. Each product contains a user generated Q&A section and is given a star rating based on customer reviews',
+    github: 'https://github.com/hackreactor-lax45-frontendcapstone/project-catwalk',
+    url: '',
+  },
+}
