015a5b6e809a7b227a9b199c0018e5648e67feb7
Tyler Jones <tylerj.audio94@gmail.com>
2021-11-14 07:07:27 -0800

    More initial CSS styling, Google font added

--- components/title/TitleCard.jsx
+++ components/title/TitleCard.jsx
@@ -6,7 +6,7 @@ const TitleCard = () => {
     <div className="title_card" id="title_card">
       <h1>Tyler Jones</h1>
       <h4>Software Developer</h4>
-      <h4>&</h4>
+      {/* <h4>&</h4> */}
       <h4>Audio Engineer</h4>
     </div>
   );
