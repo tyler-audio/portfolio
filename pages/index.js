5fb65ed58227b4e97d935c0b5137421258e1e656
Tyler Jones <tylerj.audio94@gmail.com>
2021-12-11 11:34:13 -0800

    added attributes to video tag

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ const HomePage = () => {
           autoPlay={true}
           loop={true}
           muted={true}
+          disablePictureInPicture={true}
+          disableRemotePlayback={true}
+          playsInLine={true}
         />
     </section>
   )
